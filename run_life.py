5ecf9d055d7775842fa66370f9b38c65779825e3
Filipe-p <filipe.p@soltroiavillas.com>
2020-05-29 13:08:17 +0100

    created Dog class, added some method and a constructor class to give the instances names

--- run_life.py
+++ run_life.py
@@ -3,4 +3,26 @@
 
 from dog_class import *
 
+# Initialize a Dog object -
+max_dog_instance = Dog()
+ringo_dog_instance = Dog('Ringo')
 
+print(max_dog_instance)
+
+print(max_dog_instance.name)
+print(ringo_dog_instance.name)
+
+# # Call the method .bark() on object
+#
+# print(max_dog_instance.eat('BONE'))
+# print(max_dog_instance.bark())
+# print(max_dog_instance.fetch())
+# print(max_dog_instance.potty())
+# print(max_dog_instance.bark())
+# print(max_dog_instance.sleep())
+#
+# print('walk the dog home')
+#
+# print(max_dog_instance.sleep())
+# print(max_dog_instance.sleep())
+# print(max_dog_instance.bark("Creepy Stranger!!"))
